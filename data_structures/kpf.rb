efcfb0d4386dc365c553a6c3baf93b86fc2636ae
kkossally <kkossally@gmail.com>
2019-08-16 15:45:57 -0400

    Define #valid_move?

--- data_structures/kpf.rb
+++ data_structures/kpf.rb
@@ -16,25 +16,34 @@ class KnightPathFinder
   end 
 
   def self.valid_moves(pos) # returns an array of possible positions [[0,0], [1,0]...]
-    valid_moves = [[0,1], [1,0], [0,3], [1,3], [4,1], [4,3], [3,4], [1,4]]
+    # x and y must be between 0 and 7 inclusive
+    # pos[0] - 
+    valid_moves = [[3,5], [2,4], [2,2], [3,1], [5, 1], [6, 2], [6,4], [5, 5]] # start pos is [4, 3]
     valid_moves
-  end 
+  end
 
-  def new_move_positions(pos)  
-    # calls ::valid_moves to get array of possible moves  
+  def valid_move?(new_pos) # return true if new_pos is valid
+    x0, y0 = start_pos
+    x1 , y1 = new_pos
+    # return false if x or y is not between 0 and 7 inclusive
+    return false if !x1.between?(0, 7) || !y1.between?(0, 7)
+    (x1 - x0).abs + (y1 - y0).abs == 3
+  end
+
+  def new_move_positions(pos)  # returns an array of new moves not in considered postions and then adds them to consiered positions
     new_moves = KnightPathFinder.valid_moves(pos).reject {|move| considered_pos.include?(move) } # position will be current position 
-    # filters out any positions that exists in considered_pos 
-    # returns remaining positions 
     considered_pos.concat(new_moves)
+    new_moves
   end 
 
 end
 
 if $PROGRAM_NAME == __FILE__
-  kpf = KnightPathFinder.new([2,2])
+  kpf = KnightPathFinder.new([4, 3])
   
-  p kpf.new_move_positions([2,2])
-  p kpf.considered_pos
+  # p kpf.new_move_positions([4, 3])
+  # p kpf.considered_pos
 
-
+  p kpf.valid_move?([3, 5])
+  p kpf.valid_move?([6, 3])
 end
