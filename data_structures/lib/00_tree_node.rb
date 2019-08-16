f38168a6ec42a132c1411b43cdff21b3c06b9d7c
kkossally <kkossally@gmail.com>
2019-08-16 12:09:10 -0400

    Reassign parent and children for all related nodes when we update a node's parent

--- data_structures/lib/00_tree_node.rb
+++ data_structures/lib/00_tree_node.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 class PolyTreeNode
   
   def initialize(value)
@@ -18,8 +19,37 @@ class PolyTreeNode
     @value
   end
 
-  def parent=(parent)
-    @parent = parent
+  def parent=(new_parent)
+    if !parent.nil?
+      self.parent.remove_child(self)
+    end
+
+    @parent = new_parent
+    
+    if !new_parent.nil?
+      new_parent.children = self
+    end
+  end
+
+  def remove_child(child) # removes from parent 
+    puts "Removing #{child.value}"
+  end 
+
+  def children=(child)
+    if !children.include?(child)
+      @children << child
+    end
   end
 
 end
+
+branch =PolyTreeNode.new(5)
+root = PolyTreeNode.new(2)
+branch.parent = root # add branch to roots children
+
+root.remove_child(branch)
+
+# other_root = PolyTreeNode.new(3)
+
+
+# PolyTreeNode.remove_child(root, branch) # root's children will be empty
