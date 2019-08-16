96af79d786882cebfe54e7de0cfcbaa03a0a8205
kkossally <kkossally@gmail.com>
2019-08-16 11:27:35 -0400

    Define getter and seetter methods

--- data_structures/lib/00_tree_node.rb
+++ data_structures/lib/00_tree_node.rb
@@ -1,11 +1,25 @@
 class PolyTreeNode
   
-  attr_accessor :value, :parent, :children
-
   def initialize(value)
     @value = value
     @parent = nil
     @children = []
+  end
+
+  def parent
+    @parent
+  end
+
+  def children
+    @children
+  end
+
+  def value
+    @value
+  end
+
+  def parent=(parent)
+    @parent = parent
   end
 
 end
