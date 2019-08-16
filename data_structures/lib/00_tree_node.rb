1963e425be04c977615551007671a6febbf004eb
kkossally <kkossally@gmail.com>
2019-08-16 14:37:05 -0400

    Implement dfs

--- data_structures/lib/00_tree_node.rb
+++ data_structures/lib/00_tree_node.rb
@@ -36,11 +36,19 @@ class PolyTreeNode
     else
       child.parent = self
     end
+  end
 
-    # unless children.include?(child)
-    #   @children << child
-    #   child.parent = self
-    # end
+  def dfs(target_value)
+    return self if self.value == target_value
+    children.each do |child|
+      result = child.dfs(target_value)
+      return result unless result.nil?
+    end 
+    nil 
+  end
+
+  def inspect
+    @value.inspect
   end
 
 end
@@ -49,15 +57,29 @@ if $PROGRAM_NAME == __FILE__
   a = PolyTreeNode.new('a')
   b = PolyTreeNode.new('b')
   c = PolyTreeNode.new('c')
-  d = PolyTreeNode.new('d')
-  e = PolyTreeNode.new('e')
+  # d = PolyTreeNode.new('d')
+  # e = PolyTreeNode.new('e')
+  # f = PolyTreeNode.new('f')
+  # g = PolyTreeNode.new('g')
 
-  b.parent = a # a:b 
-  a.children = c # { a: [b,c]}
-  b.parent = d # { d: [b] , a: [c] }   
-  b.add_child(e)
+  # b.parent = a # a:b 
+  # c.add_child(a) # { c: a} 
+  # b.parent = d # { d: [b] , c: [a] }  
 
-  puts "a children: #{a.children.map(&:value)}"
-  puts "d children: #{d.children.map(&:value)}" 
-  puts "b children: #{b.children.map(&:value)}" 
+  a.add_child(b)
+  a.add_child(c)
+  # b.add_child(d)
+  # b.add_child(e)
+  # c.add_child(f)
+  # f.add_child(g)
+
+  p a.dfs("c")
+  # puts "a children: #{a.children.map(&:value)}"
+  # puts "b children: #{b.children.map(&:value)}"
+  # puts "c children: #{c.children.map(&:value)}"
+  # puts "d children: #{d.children.map(&:value)}" 
+  # puts "e children: #{e.children.map(&:value)}" 
+  # puts "f children: #{f.children.map(&:value)}" 
+  # puts "g children: #{g.children.map(&:value)}" 
+  
 end
