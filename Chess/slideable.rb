37e8374e95fef1386ec275c48b1e1cad5f3a95fd
kkossally <kkossally@gmail.com>
2019-08-19 18:04:21 -0400

    Map out remaining functions of slideable module

--- Chess/slideable.rb
+++ Chess/slideable.rb
@@ -1,13 +1,18 @@
 module Slideable
-  def
-
+  def horizontal_dirs 
+    HORIZONTAL_DIRS
+  end
+  
+  def diagonal_dirs 
+    Slideable.DIAGONAL_DIRS
   end
 
   private
+  attr_reader :HORIZONTAL_DIRS, :DIAGONAL_DIRS
 
   horizontal = []
   (-7..7).each {|delta| horizontal << [0, delta] unless delta == 0}
-  
+
   vertical = []
   (-7..7).each {|delta| vertical << [delta, 0] unless delta == 0}
 
@@ -30,3 +35,15 @@ module Slideable
 
 
 end
+
+# current class constants hold too much info, they should just be the moves
+# one square away
+
+# gorw unblocked moves will return an array of all the possible moves
+# to the edge of the board in one dir
+
+# move_dirs is an almost empty module method that will be overwritten by the
+# piece's actual move pattern, but its job is to tell us what direction we can
+# move in before grow builds the arrays
+
+# move will put all that together to return all the valid moves
