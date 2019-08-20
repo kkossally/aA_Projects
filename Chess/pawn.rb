46761ce1634b2bb153e45205a3249a8b4fb9a15d
kkossally <kkossally@gmail.com>
2019-08-20 14:38:49 -0400

    Define Pawn

--- Chess/pawn.rb
+++ Chess/pawn.rb
@@ -1,6 +1,7 @@
 require_relative 'piece'
 
 class Pawn < Piece
+  attr_reader :color, :pos
 
   def symbol
     :P 
@@ -14,15 +15,21 @@ class Pawn < Piece
   private 
 
   def at_start_row? 
+    return true if color == :white && pos[0] == 6
+    return true if color == :black && pos[0] == 1
+    false 
   end
 
   def forward_dir
+    color == :white ? -1 : 1 
   end
 
   def forward_steps 
+    [forward_dir, 0]
   end
 
   def side_attacks
+    [forward_dir, forward_dir]
   end 
 
 
