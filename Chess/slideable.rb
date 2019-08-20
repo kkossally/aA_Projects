199b83fbcb30b8a82318d68c124f09c040ca79f5
kkossally <kkossally@gmail.com>
2019-08-20 11:32:22 -0400

    Define Slideable#moves

--- Chess/slideable.rb
+++ Chess/slideable.rb
@@ -1,6 +1,6 @@
 module Slideable
   def horizontal_dirs 
-    HORIZONTAL_DIRS
+    Slideable.HORIZONTAL_DIRS
   end
   
   def diagonal_dirs 
@@ -8,16 +8,18 @@ module Slideable
   end
 
   def moves
-    # move will put all that together to return all the valid moves
+    all_moves = []
+    move_dirs.each do |dir|
+      all_moves << grow_unblocked_moves_in_dir(dir[0], dir[1])
+    end
+    all_moves
   end
 
   private
   attr_reader :HORIZONTAL_DIRS, :DIAGONAL_DIRS
 
-  HORIZONTAL_DIRS = [ [-1, 0], [1, 0], [0, -1] [0, 1] ]
-
+  HORIZONTAL_DIRS = [ [-1, 0], [1, 0], [0, -1], [0, 1] ]
   DIAGONAL_DIRS = [ [-1, -1], [-1, 1], [1, -1], [1, 1] ]
-
 
   def move_dirs
     HORIZONTAL_DIRS + DIAGONAL_DIRS
@@ -30,21 +32,4 @@ module Slideable
     end
     move_list
   end
-  # horizontal = []
-  # (-7..7).each {|delta| horizontal << [0, delta] unless delta == 0}
-
-  # vertical = []
-  # (-7..7).each {|delta| vertical << [delta, 0] unless delta == 0}
-
-  # down_right = []
-  # (1..7).each { |delta| down_right << [delta, delta]}
-  
-  # up_left = []
-  # (1..7).each { |delta| up_left << [-delta, -delta]}
-  
-  # down_left = []
-  # (1..7).each { |delta| down_left << [delta, -delta]}
-  
-  # up_right = []
-  # (1..7).each { |delta| up_right << [-delta, delta]}
 end
