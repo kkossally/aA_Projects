b2aa49beaed09d65fd4ad040e20e8c6f214dd416
kkossally <kkossally@gmail.com>
2019-08-20 12:03:44 -0400

    Define Piece class skeleton

--- Chess/piece.rb
+++ Chess/piece.rb
@@ -1,9 +1,35 @@
-require_relative 'slideable'
-
 class Piece 
-  include Slideable
 
-  def initialize
+  def initialize(color, board, pos)
+    @color = color
+    @board = board
+    @pos = pos
+  end
+
+  def to_s
+    symbol.to_s
+  end
+
+  def empty?
+
+  end
+
+  def valid_moves
+
+  end
+
+  def pos=(val)
+    @pos = val
+  end
+
+  def symbol
+
+  end
+
+  private
+
+  def move_into_check?(end_pos)
+
   end
 
 end
