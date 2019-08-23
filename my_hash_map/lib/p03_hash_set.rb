8f67f9e847f9f05b02cb05bae77047d4d1ba6b17
kkossally <kkossally@gmail.com>
2019-08-23 14:44:38 -0400

    Complete Phase III

--- my_hash_map/lib/p03_hash_set.rb
+++ my_hash_map/lib/p03_hash_set.rb
@@ -1,24 +1,51 @@
 class HashSet
-  attr_reader :count
+  attr_reader :count, :store
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { Array.new }
     @count = 0
   end
 
+  def hash_key(key)
+    key.hash
+  end
+  
   def insert(key)
+    h_key = hash_key(key)
+
+    if include?(key)
+
+    else
+      if count + 1 > num_buckets
+        resize!
+      end
+
+      self[key] << h_key
+      @count += 1
+      return include?(key)
+    end
+    false
   end
 
   def include?(key)
+    h_key = hash_key(key)
+    self[key].include?(h_key)
   end
 
   def remove(key)
+    h_key = hash_key(key)
+    if include?(key)
+      self[key].delete(h_key)
+      @count -= 1
+    end
+    include?(key)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num.hash % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +53,12 @@ class HashSet
   end
 
   def resize!
+    new_set = HashSet.new(num_buckets * 2)
+    store.each do |bucket|
+      bucket.each do |el|
+        new_set.insert(el)
+      end
+    end
+    @store = new_set.store
   end
 end
