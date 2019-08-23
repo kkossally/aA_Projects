820c94241c609458963f6f60043f342761423e03
kkossally <kkossally@gmail.com>
2019-08-23 14:08:31 -0400

    Complete Phase II

--- my_hash_map/lib/p02_hashing.rb
+++ my_hash_map/lib/p02_hashing.rb
@@ -4,11 +4,18 @@ end
 
 class Array
   def hash
+    temp = []
+    each_with_index do |el, idx|
+      temp << el.hash * idx.hash
+    end
+    temp.sum + length.hash
   end
 end
 
 class String
   def hash
+    alpha = ('a'..'z').to_a
+    split("").map { |char| alpha.index(char.downcase).hash }.hash
   end
 end
 
@@ -16,6 +23,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    to_a.flatten.map { |el| el.hash }.sort.hash
   end
 end
