aea255e3ed1e90d343d912be344a7963efa6c8ff
App Academy Student <aa@Apps-Mac-mini-110.local>
2019-09-04 16:10:20 -0400

    Define current user method

---
+++ ninety_nine_cats_auth/app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+    render :new
+  end
+
+  def create
+    user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if user
+      session[:session_token] = user.reset_session_token!
+      redirect_to cats_url
+    else
+      flash.now[:errors] = ["Invalid credentials"]
+      render :new
+    end
+  end
+
+  def destroy
+    current_user.reset_session_token! if current_user
+    session[:session_token] = nil
+  end
+
+end
